a0848e424d8d4350b102266676dfdced98c821e3
Dhiraj Yadav <63800057+dhiraj-ydv@users.noreply.github.com>
2021-05-18 13:41:32 +0530

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,19 @@
+<?php
+//get data from form  
+$name = $_POST['name'];
+$email= $_POST['email'];
+$message= $_POST['message'];
+
+$to = "dhirajy108@mail.com";
+
+$subject = "Mail From website";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+
+$headers = "From: noreply@dhirajyadav.ninja" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:thankyou.html");
+?>
