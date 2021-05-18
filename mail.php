dfdc95051f694bb6532b21517068aab0b9aa8c07
Dhiraj Yadav <63800057+dhiraj-ydv@users.noreply.github.com>
2021-05-18 13:56:58 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -2,12 +2,13 @@
 //get data from form  
 $name = $_POST['name'];
 $email= $_POST['email'];
+$subject= $_POST['subject'];
 $message= $_POST['message'];
 
 $to = "dhirajy108@mail.com";
 
 $subject = "Mail From website";
-$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Subject =" . $subject . "\r\n Message =" . $message;
 
 $headers = "From: noreply@dhirajyadav.ninja" . "\r\n" .
 "CC: somebodyelse@example.com";
